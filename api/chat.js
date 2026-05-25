7c6df9a6f22b2c28d665774b79058fe5c057ac47
DropisApp <alfonsocamacho00089@gmail.com>
2026-05-24 21:39:41 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -166,7 +166,7 @@ export default async function handler(req, res) {
 
 
 
-const urlGemini = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+const urlGemini = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
 
 
 
