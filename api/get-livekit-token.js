d2d15b01f01617a9a5c0b62e4648d18ba4a4f122
DropisApp <alfonsocamacho00089@gmail.com>
2026-06-04 20:09:13 -0400

    Update get-livekit-token.js

--- api/get-livekit-token.js
+++ api/get-livekit-token.js
@@ -1,29 +1,26 @@
-// Cambia la primera línea por esto:
 const livekit = require('livekit-server-sdk');
-const AccessToken = livekit.AccessToken;
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Solo permitido POST' });
   }
 
   const { roomName, identity } = req.body;
-
-  // Vercel lee automáticamente las llaves que pusiste en Environment Variables
   const apiKey = process.env.LIVEKIT_API_KEY;
   const apiSecret = process.env.LIVEKIT_API_SECRET;
 
-  if (!apiKey || !apiSecret) {
-    return res.status(500).json({ error: 'Faltan credenciales de LiveKit en Vercel' });
+  try {
+    const at = new livekit.AccessToken(apiKey, apiSecret, { identity: identity });
+    at.addGrant({
+      roomJoin: true,
+      room: roomName,
+      canPublish: true,
+      canSubscribe: true,
+    });
+
+    const token = await at.toJwt();
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  const at = new AccessToken(apiKey, apiSecret, { identity: identity });
-  at.addGrant({
-    roomJoin: true,
-    room: roomName,
-    canPublish: true,
-    canSubscribe: true,
-  });
-
-  const token = await at.toJwt();
-  res.status(200).json({ token });
 }
